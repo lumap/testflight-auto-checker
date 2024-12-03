94560876da4395a7346e6e64b02bf788a9cad2d9
lumap <lumap@duck.com>
2024-12-03 15:22:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+TESTFLIGHT_LINK = ""
+INTERVAL = 60 # in seconds
+
+import time
+import requests
+import os
+
+print("Hello!")
+while True:
+    response = requests.get(TESTFLIGHT_LINK)
+    if (response.status_code != 200):
+        print("Error: " + str(response.status_code))
+    if (response.content.find(b"This beta is full") != -1):
+        print("Beta is full")
+    else:
+        print("Beta is not full. Opening in ur favorite browser...")
+        os.system("open " + TESTFLIGHT_LINK)
+        os.system("say 'Testflight link is not full.'")
+        break
+            
+    print("eepy time...\n")
+    time.sleep(INTERVAL)
+
+print("Goodbye :(")
